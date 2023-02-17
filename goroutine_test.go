cc881680888ce81bb1bd92adee3ba63813c9ac89
rizky.pratama <rizky.pratama@sicepat.com>
2023-02-17 10:30:40 +0700

    go routine itu ringan brow

--- goroutine_test.go
+++ goroutine_test.go
@@ -20,3 +20,15 @@ func TestCreateGoroutine(t *testing.T) {
 	// Sleep digunakan sebagai waktu jeda untuk menunggu proses goroutine diatas selesai
 	time.Sleep(1 * time.Second)
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Display ", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
